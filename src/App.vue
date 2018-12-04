0e0902843469e9c2b0aa835c1dff428bed85e695
Lac <laclys@126.com>
2018-12-04 23:34:22 +0800

    fix tab style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <v-header :seller="seller"></v-header>
-    <tab></tab>
+    <div class="tab-wrapper" >
+      <tab></tab>
+    </div>
   </div>
 </template>
 
@@ -34,4 +36,11 @@ export default {
 };
 </script>
 <style lang="stylus">
+  #app
+    .tab-wrapper
+      position: fixed
+      top: 136px
+      left: 0
+      right 0
+      bottom 0
 </style>
